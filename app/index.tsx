77319be82ec7b0208a16c3e59c857ffa4dd99ad4
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-07-10 22:51:28 +0800

    feat: npx expo install nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,14 @@
 import { Text, View } from "react-native";
 import { useGames } from "@/hooks/useGames";
+import "./global.css";
 
 export default function Index() {
   // eslint-disable-next-line
   const { games } = useGames();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
     </View>
   );
 }
