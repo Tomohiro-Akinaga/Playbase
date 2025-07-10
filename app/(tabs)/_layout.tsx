07c16d8824f4368206f9baaf76cd6b38fe63fa9f
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-07-10 23:35:07 +0800

    feat: tabBarIconのコンポーネント化

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,25 @@
 import { Tabs } from "expo-router";
+import TabBarIcon from "../components/TabBar/TabBarIcon";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="search" />
+    <Tabs screenOptions={{ tabBarShowLabel: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "index",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <TabBarIcon title="Home" focused={focused} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <TabBarIcon title="Search" focused={focused} />,
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/components/TabBar/TabBarIcon.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+interface Props {
+  title: string;
+  focused: boolean;
+}
+
+export default function TabBarIcon({ title, focused }: Props) {
+  if (focused) return <Text>focused!!!!</Text>;
+  return <Text>{title}</Text>;
+}
